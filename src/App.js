f5c7de4662a5441d3ba38dd01efa88a9020624b7
aateplyakovva <anna.teplyakovap@gmail.com>
2021-11-05 12:24:28 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import 'swiper/swiper.min.css';
 import './App.scss';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Routes from './config/Routes';
 
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+        <Route render={props => (
+          <>
+            <Header {...props}/>
+            <Routes/>
+            <Footer/>
+          </>
+        )}/>
+    </BrowserRouter>
   );
 }
 
