ff87203625db7106dde78cf723e0331b33bcf437
aateplyakovva <anna.teplyakovap@gmail.com>
2021-11-05 17:13:44 +0300

    header done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,7 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
+
 import { Link, useLocation } from 'react-router-dom';
+
 import './header.scss';
 
 import logo from '../../assets/moviet.png';
@@ -15,21 +17,32 @@ const headerNav = [
     },
 ];
 
-
 const Header = () => {
 
-    const {pathname} = useLocation();
+    const { pathname } = useLocation();
     const headerRef = useRef(null);
 
-
     const active = headerNav.findIndex(e => e.path === pathname);
+
+    useEffect(() => {
+        const shrinkHeader = () => {
+            if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+                headerRef.current.classList.add('shrink');
+            } else {
+                headerRef.current.classList.remove('shrink');
+            }
+        }
+        window.addEventListener('scroll', shrinkHeader);
+        return () => {
+            window.removeEventListener('scroll', shrinkHeader);
+        };
+    }, []);
 
     return (
         <div ref={headerRef} className="header">
             <div className="header__wrap container">
                 <div className="logo">
                     <img src={logo} alt="" />
-                    <Link to="/"></Link>
                 </div>
                 <ul className="header__nav">
                     {
@@ -44,7 +57,7 @@ const Header = () => {
                 </ul>
             </div>
         </div>
-    )
+    );
 }
 
 export default Header;
