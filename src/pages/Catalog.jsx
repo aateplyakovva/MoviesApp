0c3cf14e74ef13fd5c5ad9528b9b691dfab3dbcd
aateplyakovva <anna.teplyakovap@gmail.com>
2021-11-07 18:05:55 +0300

    add movies grid

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+
+import { useParams } from 'react-router';
+
+import PageHeader from '../components/page-header/PageHeader';
+import { category as cate } from '../api/tmdbApi';
+import MovieGrid from '../components/movie-grid/MovieGrid';
 
 const Catalog = () => {
+
+    const { category } = useParams(); 
+
     return (
-        <div>
-            Catalog
-        </div>
+        <>
+            <PageHeader>
+                {category === cate.movie ? 'Movies' : null}
+            </PageHeader>
+            <div className="container">
+                <div className="section mb-3">
+                    <MovieGrid  category={category}/>
+                </div>
+            </div>
+        </>
     )
 }
 
