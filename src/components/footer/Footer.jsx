07d79d197172173fb4c5db1127397e6abbb9be88
aateplyakovva <anna.teplyakovap@gmail.com>
2021-11-07 16:45:16 +0300

    add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './footer.scss';
+import { Link } from 'react-router-dom';
+import bg from '../../assets/footer-bg.jpg';
+import logo from '../../assets/moviet.png';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className="footer" style={{backgroundImage: `url(${bg})`}}>
+            <div className="footer__content container">
+                <div className="footer__content__logo">
+                    <div className="logo">
+                        <Link to={'/'}>
+                        <img src={logo} alt="" />
+                        </Link>
+                    </div>
+                </div>
+            </div>  
         </div>
     )
 }
